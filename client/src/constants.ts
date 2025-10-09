161a6e8d0fad52aa8fd89abcddc9c8621cb1d1c7
fefacio <ticorossetti2002@hotmail.com>
2025-10-09 00:20:22 -0300

    Updated calculation to place square inside circle for facility node display

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_RADIUS_SIZE = 20;
-export const DEFAULT_FACILITY_WIDTH = 20;
-export const DEFAULT_FACILITY_HEIGHT = 20;
+export const DEFAULT_FACILITY_WIDTH = Math.floor(DEFAULT_RADIUS_SIZE*Math.sqrt(2)*0.95);
+export const DEFAULT_FACILITY_HEIGHT = Math.floor(DEFAULT_RADIUS_SIZE*Math.sqrt(2)*0.95);
